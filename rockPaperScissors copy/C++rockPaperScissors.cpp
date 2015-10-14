551c18516f90d1f4b87aa0f669eaab5ed2506fdd
Ian Conery <ianconery@gmail.com>
2015-10-13 17:26:06 -0700

    still adding to c++

--- rockPaperScissors copy/C++rockPaperScissors.cpp
+++ rockPaperScissors copy/C++rockPaperScissors.cpp
@@ -7,12 +7,12 @@ using namespace std;
 string result_builder ()
 {
  vector<string> throws {"rock", "paper", "scissors"}; 
- vector<string> result;
+ vector<vector<string>> result;
 
  for(int i = 0; i < 3; i++){
   for(int j = 0; j < 3; j++){
     for(int k = 0; k < 3; k++){
-      result.push_back(vector<string> {throws[i], throws[j], throws[k]})
+      result.push_back({throws[i], throws[j], throws[k]});
     }
   }
  }
@@ -21,6 +21,6 @@ string result_builder ()
 
 void main ()
 {
-  string result_builder();
+  cout << result_builder();
 
 }
